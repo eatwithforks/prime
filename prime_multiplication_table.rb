4575fee1a98b4d3df23ac932e44260b8008756c5
Jye Lee <jlee@cloudpassage.com>
2016-07-10 12:58:54 -0700

    rename table_data to table

--- prime_multiplication_table.rb
+++ prime_multiplication_table.rb
@@ -7,8 +7,8 @@ class PrimeMultiplication
     primes = PrimeController.list_primes(range)
     raise "range var: #{range} is invalid." unless primes.class.eql? Array
 
-    table_data = PrimeController.create_table(primes)
-    PrimeController.print_formatted_table(table_data)
+    table = PrimeController.create_table(primes)
+    PrimeController.print_formatted_table(table)
   end
 end
 
